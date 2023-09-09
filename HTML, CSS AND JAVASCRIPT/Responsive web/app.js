de64e750612354a7834b44af42165d1cc7905dd9
DonMizzy <131966450+DonMizzy@users.noreply.github.com>
2023-09-09 17:58:56 +0100

    with only html, css and js

---
+++ HTML, CSS AND JAVASCRIPT/Responsive web/app.js
@@ -0,0 +1,54 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+const navLogo = document.querySelector('#navbar_logo');
+
+//display mobile menu
+const mobileMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+}
+
+menu.addEventListener("click", mobileMenu);
+
+// Show active menu when scrolling//
+const highlightMenu = () => {
+  const elem = document.querySelector('.highlight')
+  const homeMenu = document.querySelector('#home-page')
+  const aboutMenu = document.querySelector('#about-page')
+  const servicesMenu = document.querySelector('#services-page')
+  let scrollPos = window.scrollY
+
+  // ads 'highlight' class to menu items
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    servicesMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    servicesMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  }
+
+  if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove('highlight');
+  }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+// Close mobile Meny when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active')
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.remove('active')
+  }
+}
+menuLinks.addEventListener('click, hideMobileMenu');
+navLogo.addEventListener('click, hideMobileMenu');
